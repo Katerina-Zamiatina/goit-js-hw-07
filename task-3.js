55603083a3a53a7278ac53da30ac7d22325de378
Katerina Zamiatina <eozamyatina@gmail.com>
2020-12-16 17:44:01 +0200

    Добавляет задание 3

---
+++ task-3.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryList = document.querySelector('#gallery');
+galleryList.classList.add('gallery');
+const createGallery = images.map(image => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+  const galleryImage = document.createElement('img');
+  galleryImage.setAttribute('src', image.url);
+  galleryImage.setAttribute('alt', image.alt);
+  galleryImage.setAttribute('width', 370);
+  galleryItem.appendChild(galleryImage);
+  galleryList.appendChild(galleryItem);
+  return galleryList;
+});
+console.log(galleryList);
